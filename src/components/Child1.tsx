5d75d9b99da9b8682978be1d040f20ebc4d9afa7
borashisan <tomochuro731@gmail.com>
2023-02-15 09:11:18 +0900

    useCallback

--- src/components/Child1.tsx
+++ src/components/Child1.tsx
@@ -1,21 +1,29 @@
-import { memo } from 'react'
-import { Child2 } from './Child2'
-import { Child3 } from './Child3'
+import { type FC, memo } from "react";
+import { Child2 } from "./Child2";
+import { Child3 } from "./Child3";
 
 const style = {
-  height: '200px',
-  backgroundColor: 'lightblue',
-  padding: '8px'
-}
+  height: "200px",
+  backgroundColor: "lightblue",
+  padding: "8px",
+};
 
-export const Child1 = memo(() => {
-  console.log('Child1 レンダリング')
+type Reset = () => void;
+type Child1Props = {
+  onClickReset: Reset;
+};
+
+export const Child1: FC<Child1Props> = memo((props) => {
+  console.log("Child1 レンダリング");
+
+  const { onClickReset } = props;
 
   return (
     <div style={style}>
       <p>Child1</p>
+      <button onClick={onClickReset}>リセット</button>
       <Child2 />
       <Child3 />
     </div>
-  )
-})
+  );
+});
